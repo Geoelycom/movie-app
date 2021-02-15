aff9b8259964b12fbe3b5a80355b154cf4b85c69
EKENIMOH ELYAN <ekenimohelyan@EKENIMOHs-MacBook-Pro.local>
2021-02-15 21:30:34 +0100

    initial commit to the code

---
+++ index.js
@@ -0,0 +1,69 @@
+const fetchData = async(searchItem) => {
+    const response = await axios.get(' http://www.omdbapi.com/', {
+        params: {
+            apikey: '85c1cc6e',
+            s: searchItem
+        }
+
+    });
+    if (response.data.Error) {
+        return [];
+    }
+
+    return response.data.Search;
+};
+
+
+const onMovieSelect = async movie => {
+    const response = await axios.get(' http://www.omdbapi.com/', {
+        params: {
+            apikey: '85c1cc6e',
+            i: movie.imdbID
+        }
+    });
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = movieDetail => {
+    return `
+        <article class="media">
+          <figure class="media-left">
+            <p class="image">
+              <img src="${movieDetail.Poster}" />
+        </p>
+     </figure>
+    <div class="media-content">
+     <div class="content">
+        <h1>${movieDetail.Title}</h1>
+        <h4>${movieDetail.Genre}</h4>
+        <p>${movieDetail.Plot}</p>
+    </div>
+</div>
+            </article>
+            <article class="notification is-primary">
+            <p class="title">${movieDetail.Awards}</p>
+            <p class="subtitle">Awards</p>
+            </article>
+
+            <article class="notification is-primary">
+            <p class="title">${movieDetail.BoxOffice}</p>
+            <p class="subtitle">Box Office</p>
+            </article>
+
+            <article class="notification is-primary">
+            <p class="title">${movieDetail.Metascore}</p>
+            <p class="subtitle">Metascore</p>
+            </article>
+
+            <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbRating}</p>
+            <p class="subtitle">IMDB Rtaings</p>
+            </article>
+
+            <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbvotes}</p>
+            <p class="subtitle">IMDB Votes</p>
+            </article>
+
+    `;
+};
